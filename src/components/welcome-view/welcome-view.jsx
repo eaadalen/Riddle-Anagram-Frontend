ce51d49c66bf2c012be012fe666a23df2dd79a76
eaadalen <eaadalen112@gmail.com>
2024-05-27 22:42:29 -0500

    Updates

--- src/components/welcome-view/welcome-view.jsx
+++ src/components/welcome-view/welcome-view.jsx
@@ -5,10 +5,10 @@ import { Link } from "react-router-dom";
 export const WelcomeView = () => {
   return (
     <div className="custom-container">
-        <div className="left">Spelling Survival</div>
+        <div className="left">Riddle Scramble</div>
         <div className="right"> 
           <Link to={`/play`}>
-              <p className="button">Start</p>
+              <p className="button">Enter</p>
           </Link>
         </div>
     </div>
